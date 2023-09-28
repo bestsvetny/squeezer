29d51be5d75b8dbd1aa6101addf51314358744a6
Arthur <stfriman13@gmail.com>
2023-09-28 21:26:37 +0300

    build: env-vars fixes

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,7 +4,7 @@ import { AllExceptionsFilter } from 'src/all-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.API_PORT || 5000;
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
--- apps/core/src/widgets/chat-widget/model/createChatSocket.ts
+++ apps/core/src/widgets/chat-widget/model/createChatSocket.ts
@@ -1,10 +1,10 @@
 import { io } from 'socket.io-client';
-import { API_URL, CHAT_HOST_URL } from 'shared/constants';
+import { CHAT_HOST_URL } from 'shared/constants';
 
 export const createChatSocket = () => {
     if (process.env.NODE_ENV === 'production') {
         return io(CHAT_HOST_URL, { path: '/chat/api', addTrailingSlash: false });
     } else {
-        return io(API_URL);
+        return io(CHAT_HOST_URL);
     }
 };
